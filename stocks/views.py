e849796f9a68c84eb9b6a0d9f7bdfd6a517b76d9
Ajay Borse <borseajay266@gmail.com>
2026-08-07 18:34:04 +0530

    Integrate Yahoo Finance for Indian stock prices

--- stocks/views.py
+++ stocks/views.py
@@ -1,15 +1,17 @@
-from decouple import config
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import yfinance as yf
 
 
 class StockPriceView(APIView):
 
     def get(self, request, symbol):
 
-        api_key = config("TWELVE_DATA_API_KEY", default="NOT_FOUND")
+        stock = yf.Ticker(symbol)
+
+        info = stock.fast_info
 
         return Response({
-            "symbol": symbol.upper(),
-            "api_key": api_key
+            "symbol": symbol,
+            "price": info.get("lastPrice")
         })
