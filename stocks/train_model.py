95193c9d17f58b0da33252bfc6217baccaa54b52
Ajay Borse <borseajay266@gmail.com>
2026-08-08 02:37:26 +0530

    Train stock prediction model with Linear Regression

---
+++ stocks/train_model.py
@@ -0,0 +1,36 @@
+import joblib
+
+from sklearn.linear_model import LinearRegression
+
+from feature_engineering import create_features
+
+
+def train_model(symbol):
+
+    data = create_features(symbol)
+
+    if data is None:
+        return
+
+    # Features (Input)
+    X = data[["Close", "MA5", "MA10"]]
+
+    # Target (Output)
+    y = data["Target"]
+
+    # Create Model
+    model = LinearRegression()
+
+    # Train Model
+    model.fit(X, y)
+
+    # Save Model
+    joblib.dump(model, "stock_model.pkl")
+
+    print("\n✅ Model Trained Successfully!")
+
+    print("Model saved as stock_model.pkl")
+
+
+if __name__ == "__main__":
+    train_model("RELIANCE.NS")
